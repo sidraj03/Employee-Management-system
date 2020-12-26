9f424835aa31dfaf4f1589ca30dc235a54debdac
sidraj03 <sidharthmiglani_3@yahoo.co.in>
2020-12-26 21:09:14 +0530

    update for deployment 7

--- src/main/java/com/luv2code/springboot/thymleafdemo/controller/EmployeeController.java
+++ src/main/java/com/luv2code/springboot/thymleafdemo/controller/EmployeeController.java
@@ -31,7 +31,7 @@ public class EmployeeController {
 //		//add to the spring model
 //		theModel.addAttribute("employees",theEmployees);
 		
-		return "helloworld";
+		return "list-employees";
 	}
    	
 //	@GetMapping("/showFormForAdd")
